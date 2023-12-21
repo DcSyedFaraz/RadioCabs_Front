1feae5840d6007f32723d4c95b72cf5930270bc1
Syed <dcsyedfaraz@gmail.com>
2023-12-22 00:06:47 +0500

    register form updated

--- src/app/front/companylist/companylist.component.ts
+++ src/app/front/companylist/companylist.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, type OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 import { AuthService } from 'src/app/services/auth.service';
 
@@ -8,16 +9,45 @@ import { AuthService } from 'src/app/services/auth.service';
   standalone: true,
   imports: [
     CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   templateUrl: './companylist.component.html',
   styleUrls: ['./companylist.component.css'],
 })
 export class CompanylistComponent implements OnInit {
   dataSource: any;
+  registerForm: FormGroup;
 
-  constructor(private service: AuthService, private toastr: ToastrService) { }
+  constructor(private service: AuthService, private toastr: ToastrService, private fb: FormBuilder) {
+    this.registerForm = this.fb.group({
+      name: ['', [Validators.required]],
+      contactPerson: ['', [Validators.required]],
+      designation: ['', [Validators.required]],
+      address: ['', [Validators.required]],
+      mobile: ['', [Validators.required]],
+      telephone: ['', []],
+      faxNumber: ['', []],
+      email: ['', [Validators.required, Validators.email]],
+      membershipType: [0, []],
+    });
+  }
 
   ngOnInit(): void {
+    this.gettingdata();
+  }
+
+  onSubmit() {
+    console.log('Form submitted:', this.registerForm.value);
+
+    this.service.register(this.registerForm.value).subscribe(
+      (a:any) => {
+        console.log("Register Data Successfully ", a);
+        this.toastr.success(a.message)
+
+      }
+    );
+    this.registerForm.reset();
     this.gettingdata();
   }
 
@@ -26,7 +56,7 @@ export class CompanylistComponent implements OnInit {
       .subscribe((data) => {
         console.log(data);
 
-        this.dataSource = data ;
+        this.dataSource = data;
       },
         (error) => {
           console.error('Error fetching data:', error);
